58f3394dd6971a4216e0bb3404690b03f72fbc36
Zirdgal <zirdman@gmail.com>
2024-07-17 06:42:46 +0300

    new all cards tab

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const introContainer = document.getElementById("introduction-container");
 const keybindContainer = document.getElementById("keybinds-container");
+const cardContainer = document.getElementById("cards-container");
 
 introContainer.onclick = function() {
     location.href = 'pages/begin.html';
 };
 keybindContainer.onclick = function() {
     location.href = 'pages/keybinds.html';
+};
+cardContainer.onclick = function() {
+    location.href = 'pages/cards.html';
 };
